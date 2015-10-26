68f8e6927271f44dedd8d00c995afdaa5777e59b
Eric Kryski <e.kryski@gmail.com>
2015-10-26 14:12:21 -0600

    adding more special query params that we are supporting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 /**
  *
- *  Sets up the query properly if $limit, $skip, $sort, or $select is passed in params.
- *  Those same parameters are then removed from _conditions so that we aren't searching
- *  for data with a $limit parameter.
+ *  Sets up the query properly if any special params are passed in params.
+ *  Those same parameters are then removed from the query conditions so that
+ *  we aren't searching for data with a $limit parameter.
+ *
+ *  This insanity is in to provide a unified query interface directly from
+ *  clients that works across multiple databases.
  */
 
 export default function(query) {
@@ -10,7 +13,15 @@ export default function(query) {
     $sort: query.$sort,
     $limit: query.$limit,
     $skip: query.$skip,
-    $select: query.$select
+    $select: query.$select,
+    $or: query.$or,
+    $in: query.$in,
+    $nin: query.$nin,
+    $lt: query.$lt,
+    $lte: query.$lte,
+    $gt: query.$gt,
+    $gte: query.$gte,
+    $not: query.$not
   };
 
   // Remove the params from the query's conditions.
@@ -18,6 +29,14 @@ export default function(query) {
   delete query.$limit;
   delete query.$skip;
   delete query.$select;
+  delete query.$or;
+  delete query.$in;
+  delete query.$nin;
+  delete query.$lt;
+  delete query.$lte;
+  delete query.$gt;
+  delete query.$gte;
+  delete query.$not;
 
   return filters;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -93,4 +93,180 @@ describe('Feathers Query Filters', function() {
       expect(result.$select).to.be.undefined;
     });
   });
+
+  describe('$or', function() {
+    beforeEach(function() {
+      this.query = { $or: 1 };
+    });
+
+    it('returns $or when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$or).to.equal(1);
+    });
+
+    it('removes $or from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$or).to.be.undefined;
+    });
+  });
+
+  describe('$in', function() {
+    beforeEach(function() {
+      this.query = { $in: 1 };
+    });
+
+    it('returns $in when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$in).to.equal(1);
+    });
+
+    it('removes $in from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$in).to.be.undefined;
+    });
+  });
+
+  describe('$nin', function() {
+    beforeEach(function() {
+      this.query = { $nin: 1 };
+    });
+
+    it('returns $nin when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$nin).to.equal(1);
+    });
+
+    it('removes $nin from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$nin).to.be.undefined;
+    });
+  });
+
+  describe('$lt', function() {
+    beforeEach(function() {
+      this.query = { $lt: 1 };
+    });
+
+    it('returns $lt when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$lt).to.equal(1);
+    });
+
+    it('removes $lt from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$lt).to.be.undefined;
+    });
+  });
+
+  describe('$lte', function() {
+    beforeEach(function() {
+      this.query = { $lte: 1 };
+    });
+
+    it('returns $lte when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$lte).to.equal(1);
+    });
+
+    it('removes $lte from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$lte).to.be.undefined;
+    });
+  });
+
+  describe('$gt', function() {
+    beforeEach(function() {
+      this.query = { $gt: 1 };
+    });
+
+    it('returns $gt when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$gt).to.equal(1);
+    });
+
+    it('removes $gt from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$gt).to.be.undefined;
+    });
+  });
+
+  describe('$gte', function() {
+    beforeEach(function() {
+      this.query = { $gte: 1 };
+    });
+
+    it('returns $gte when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$gte).to.equal(1);
+    });
+
+    it('removes $gte from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$gte).to.be.undefined;
+    });
+  });
+
+  describe('$not', function() {
+    beforeEach(function() {
+      this.query = { $not: 1 };
+    });
+
+    it('returns $not when present in query', function() {
+      var result = filter(this.query);
+      expect(result.$not).to.equal(1);
+    });
+
+    it('removes $not from query when present', function() {
+      filter(this.query);
+      expect(this.query).to.deep.equal({});
+    });
+
+    it('returns undefined when not present in query', function() {
+      var query = { $foo: 1 };
+      var result = filter(query);
+      expect(result.$not).to.be.undefined;
+    });
+  });
 });
